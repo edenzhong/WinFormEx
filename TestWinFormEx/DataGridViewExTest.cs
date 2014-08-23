51eaf20c677441220fdad6be3de96b67c0fdaed9
Eden Zhong <Eden.zhong@gmail.com>
2014-08-23 13:07:55 +0800

    bak1

---
+++ TestWinFormEx/DataGridViewExTest.cs
@@ -0,0 +1,126 @@
+﻿using WinFormEx;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+
+namespace TestWinFormEx
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DataGridViewExTest and is intended
+    ///to contain all DataGridViewExTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DataGridViewExTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for DataGridViewEx Constructor
+        ///</summary>
+        [TestMethod()]
+        public void DataGridViewExConstructorTest()
+        {
+            DataGridView dgv = null; // TODO: Initialize to an appropriate value
+            DataGridViewEx target = new DataGridViewEx(dgv);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for CellEndEdit
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("WinFormEx.exe")]
+        public void CellEndEditTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            DataGridViewEx_Accessor target = new DataGridViewEx_Accessor(param0); // TODO: Initialize to an appropriate value
+            object sender = null; // TODO: Initialize to an appropriate value
+            DataGridViewCellEventArgs e = null; // TODO: Initialize to an appropriate value
+            target.CellEndEdit(sender, e);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for IsSimpleType
+        ///</summary>
+        [TestMethod()]
+        public void IsSimpleTypeTest()
+        {
+            DataGridView dgv = null; // TODO: Initialize to an appropriate value
+            DataGridViewEx target = new DataGridViewEx(dgv); // TODO: Initialize to an appropriate value
+            Type t = null; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.IsSimpleType(t);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for SetObj
+        ///</summary>
+        [TestMethod()]
+        public void SetObjTest()
+        {
+            DataGridView dgv = null; // TODO: Initialize to an appropriate value
+            DataGridViewEx target = new DataGridViewEx(dgv); // TODO: Initialize to an appropriate value
+            object obj = null; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.SetObj(obj);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
---
+++ WinFormEx/DataGridView/DataGridViewEx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace WinFormEx
+{
+    public class DataGridViewEx
+    {
+        class ExItemInfo
+        {
+            public DataGridViewCell NameCell{ get; set; }
+
+            public DataGridViewCell ValueCell{ get; set; }
+
+            public Object Tag{ get; set; }
+        }
+
+        #region internal properties
+
+        // grid view col
+        internal static int IconCol { get { return 0; } }
+
+        internal static int NameCol { get { return 1; } }
+
+        internal static int ValueCol { get { return 2; } }
+
+        internal static int TypeCol { get { return 3; } }
+
+        // icon index
+        static int BlankIconIdx { get { return 0; } }
+
+        static int CloseIconIdx { get { return 1; } }
+
+        static int OpenIconIdx { get { return 2; } }
+
+        #endregion
+
+        List<ExItemInfo> _exItemInfos;
+
+        List<ExItemInfo> ExItemInfos
+        {
+            get
+            {
+                if (null == _exItemInfos)
+                {
+                    _exItemInfos = new List<ExItemInfo>();
+                }
+                return _exItemInfos;
+            }
+            set
+            {
+                _exItemInfos = value;
+            }
+        }
+
+        DataGridView _dgv = null;
+
+        public DataGridViewEx(DataGridView dgv)
+        {
+            _dgv = dgv;
+
+            // set column title
+            _dgv.Columns.Clear();
+            _dgv.Columns.Add("Icon", "");
+            _dgv.Columns.Add("Property", "Property");
+            _dgv.Columns.Add("Value", "Value");
+            _dgv.Columns.Add("Type", "Type");
+            _dgv.Columns[0].ReadOnly = true;
+            _dgv.Columns[1].ReadOnly = true;
+            _dgv.Columns[2].ReadOnly = false; // value could be edited.
+            _dgv.Columns[3].ReadOnly = true;
+
+
+            // set event handler
+            _dgv.CellEndEdit += CellEndEdit;
+        }
+
+        public bool SetObj(Object obj)
+        {
+            _dgv.Rows.Clear();
+            _dgv.Tag = obj;
+
+            if (IsSimpleType(obj.GetType()))
+            {
+
+            }
+            /*
+            PropertyInfo[] propertyInfos;
+            Type t = obj.GetType();
+            propertyInfos = t.GetProperties();
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+                String val;
+                Object o = propertyInfo.GetValue(obj, null);
+                if (null == o)
+                {
+                    val = "";
+                }
+                else
+                {
+                    val = o.ToString();
+                }
+                ExItemInfo eii = new ExItemInfo();
+
+                _dgv.Rows.Add(propertyInfo.Name, val);
+                DataGridViewRow row = _dgv.Rows[_dgv.Rows.Count - 1];
+                row.Cells[0].Tag = eii;
+                row.Cells[1].Tag = eii;
+                eii.nameCell = row.Cells[0];
+                eii.valCell = row.Cells[1];
+
+
+                //eii.tag = 
+
+            }
+            */
+            return false;
+        }
+
+        private void CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        public bool IsSimpleType(Type t)
+        {
+            if (t.IsValueType || t == typeof(string) || t == typeof(object))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
